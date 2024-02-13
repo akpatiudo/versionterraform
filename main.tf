a776a1d3311af57f31b62263c04946e8141c481e
Ebenezer Akpati <118566096+akpatiudo@users.noreply.github.com>
2024-02-13 18:25:22 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,26 +25,23 @@ resource "azurerm_resource_group" "volvic-prod" {
   location = "sweden central"
 }
 
-# Create app service plan
-resource "azurerm_service_plan" "volvic-prod-plan" {
-  name                = "volvic-prod-Webappplan"
-  resource_group_name = azurerm_resource_group.volvic-prod.name
+# Create a virtual network
+resource "azurerm_virtual_network" "volvic-prod-vnet" {
+  name                = "volvic-prod-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.volvic-prod.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
-}
-
-# Create web app
-resource "azurerm_linux_web_app" "volvic-prod-webapp" {
-  name                = "volvic-webapp"
   resource_group_name = azurerm_resource_group.volvic-prod.name
-  location            = azurerm_service_plan.volvic-prod-plan.location
-  service_plan_id     = azurerm_service_plan.volvic-prod-plan.id
-
-  site_config {}
 }
 
-# Create network interface
+# Create a subnet within the virtual network
+resource "azurerm_subnet" "volvic-prod-subnet" {
+  name                 = "volvic-prod-subnet"
+  resource_group_name  = azurerm_resource_group.volvic-prod.name
+  virtual_network_name = azurerm_virtual_network.volvic-prod-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a network interface
 resource "azurerm_network_interface" "volvic-prod-nic" {
   name                = "volvic-prod-nic"
   resource_group_name = azurerm_resource_group.volvic-prod.name
@@ -57,9 +54,9 @@ resource "azurerm_network_interface" "volvic-prod-nic" {
   }
 }
 
-# Create Ubuntu virtual machine
+# Create a Ubuntu virtual machine
 resource "azurerm_linux_virtual_machine" "volvic-prod-vm" {
-  name                = "volvic-vm"
+  name                = "volvic-prod-vm"
   resource_group_name = azurerm_resource_group.volvic-prod.name
   location            = azurerm_resource_group.volvic-prod.location
   size                = "Standard_DS1_v2"
@@ -67,7 +64,7 @@ resource "azurerm_linux_virtual_machine" "volvic-prod-vm" {
   admin_password      = "Ukjobs##2020"  # Set the desired password here
 
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.volvic-prod-nic.id,
   ]
 
   os_disk {
